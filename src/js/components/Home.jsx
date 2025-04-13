a76dc5ea5a0933a52c489cf49908471386b76146
Pabloherresp <pabloherreraespinola@gmail.com>
2025-04-13 02:58:06 +0000

    Second solution

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import {useState,useEffect} from "react";
+import {useState,useEffect,useRef} from "react";
 
 const USER_API = "https://playground.4geeks.com/todo/users/"
 
 const Home = () => {
+	//useState
 	const [list,setList] = useState([])
 	const [text,setText] = useState("")
+	const [textName,setTextName] = useState("")
 	const [user,setUser] = useState("")
 	const [boxClass, setBox] = useState("")
+
+	//useRef que uso para que el input de la tarea recupere el foco al editar
+	const ref = useRef(null);
 
 	const createUser = async (name) => {
 		try{
@@ -16,8 +21,7 @@ const Home = () => {
 				headers: {"Content-Type": "application/json"}
 			})
 			if (!resp.ok) 	throw new Error(`Error creating user ${name} -> Error: ${resp.status} ${resp.error}`)
-			const data = await resp.json()
-			console.log(data)
+			else	alert(`A new user with the name ${name} was created.`);
 		}catch(error){
 			console.log(error)
 		}
@@ -36,6 +40,7 @@ const Home = () => {
 				}
 				const data = await resp.json()
 				setList(data.todos)
+				setText("")
 			}catch(error){
 				console.log(error)
 			}
@@ -50,7 +55,7 @@ const Home = () => {
 				headers: {"Content-Type": "application/json"},
 				body:JSON.stringify({
 					"label": text,
-					"is_done": false
+					"is_done": "false"
 				})
 			})
 			if(!resp.ok) throw new Error("Error: Couldn't create task" + resp.status)
@@ -75,6 +80,11 @@ const Home = () => {
 		}
 	}
 
+	const editTask = async (id,label) => {
+		deleteTask(id)
+		setText(label)
+	}
+	
 	const deleteTask = async (id) => {
 		try {
 			const resp = await fetch("https://playground.4geeks.com/todo/todos/"+id, {method: "DELETE"})
@@ -85,40 +95,80 @@ const Home = () => {
 		}
 	}
 
-	useEffect(()=>{setUser("pabloherresp")},[])
+	const deleteUser = async (name) => {
+		try {
+			const resp = await fetch(`https://playground.4geeks.com/todo/users/${name}`, {
+				method: "DELETE"
+			})
+			if(!resp.ok) throw new Error("Couldn't delete user -> " + resp.status)
+			else	alert(`The user ${name} was deleted.`);
+			setUser("")
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const changeUser = (e) => {
+		e.preventDefault()
+		setUser(textName)
+		setTextName("")
+	}
+	
+	//lo dejo en caso de querer iniciar con un nombre concreto
+	/*
+ 	useEffect(()=>{
+		//setUser("")
+	},[]) */
 
 	useEffect(()=>{getTodos(user)},[user])
 	
 	useEffect(()=>{
-		if(list.length > 0){
+		if(list?.length > 0){
 			setBox("box")
 			setTimeout(() => {
 				setBox("")
 			}, 200);
 		}
+		if (ref?.current) {
+			ref.current.focus();
+		 }
 	},[list])
 
 	return (
-		<div>
+		<div className="text-white">
+			{(user ? 
 			<div className="container text-center">
-				<p className="mt-3  display-5">User: {user}</p>
+				<div className="d-flex">
+					<p className="mt-3  fs-4">User: {user}</p>
+					<button type="button" className="btn btn-info ms-auto my-3" onClick={()=>setUser("")}>Load Other User</button>
+					<button type="button" className="btn btn-danger ms-auto my-3" onClick={()=>deleteUser(user)}>Delete Current User</button>
+				</div>
 				<form onSubmit={(e)=>{addTask(e)}} className="mt-5 mb-2">
-					<input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="What needs to be done?" className="form-text form-control form-control-lg"/>
+					<input ref={ref} type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="What needs to be done?" className="form-text form-control form-control-lg w-50 mx-auto" required/>
 					<input type="submit" value="" hidden/>
 				</form>
 				<ul className={boxClass + " lista mt-2 list-group list-group-flush d-inline-flex bg-transparent"}>
 					{list?.length > 0 ? list?.map((el)=>
 						<li className="lista-item d-flex justify-content-start align-items-center list-group-item fs-4 bg-transparent" key={el.id}>
-							<span className={"text-break me-auto "+(el.is_done ? "text-decoration-line-through":"")}>
+							<span className={"text-break me-auto "+(el.is_done ? "text-white-50 text-decoration-line-through":"text-white ")}>
 								{el.label}
 							</span>
-							<span className="papelera ms-3" onClick={e=>toggleTaskStatus(el.id,el.is_done)}>
+							<span className="papelera ms-3 text-white" onClick={e=>toggleTaskStatus(el.id,el.is_done)}>
 								<i className={"fa-solid " + (el.is_done ? "fa-x":"fa-check")}></i>
 							</span>
-							<span className="papelera ms-3" onClick={e=>deleteTask(el.id)}><i className="fa-solid fa-trash"></i></span>
-						</li>) : <li className="list-group-item fs-2" key="empty">There are no tasks, add some</li>}
+							<span className="papelera ms-3 text-white" onClick={e=>editTask(el.id,el.label)}><i className="fa-solid fa-pencil"></i></span>
+							<span className="papelera ms-3 text-white" onClick={e=>deleteTask(el.id)}><i className="fa-solid fa-trash"></i></span>
+						</li>) : <li className="list-group-item fs-2 bg-transparent text-white" key="empty">There are no tasks, add some</li>}
 				</ul>
 			</div>
+			:
+			<div className="container text-center">
+				<form onSubmit={(e)=>changeUser(e)} className="mt-5 mb-2">
+					<input type="text" value={textName} onChange={(e)=>setTextName(e.target.value)} placeholder="Name of the user you wish to load or create" className="form-text form-control w-50 mx-auto" required/>
+					<input type="submit" value="" hidden/>
+				</form>
+			</div>
+			)}
 		</div>
 	);
 };
